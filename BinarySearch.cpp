c414dfe217085f6b4c3bf6ad9aa41db49eb39676
abiiiaja <kalindraaraya@gmail.com>
2026-04-16 17:19:59 +0700

    query untuk input ke array di dalam looping for

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -24,7 +24,8 @@ void input(){
     cout << "===========================\n";
 
     for(int i = 0; i < nPanjang; i++){
-        
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i];
     }
 
 
